2d35773553903dcf447c31142f3b00e2b059780a
Mayun Suryatama <mayunngrh@gmail.com>
2026-07-10 11:53:19 +0800

    make the redirect more detail

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -138,7 +138,7 @@ export default function Footer() {
         {/* Map */}
         <div className="flex-1 flex justify-start pl-16">
           <a
-            href="https://www.google.com/maps?q=-8.6103,115.3197"
+            href="https://www.google.com/maps/search/Casabama+Bali/"
             target="_blank"
             rel="noopener noreferrer"
             className="w-lg h-56 shrink-0 cursor-pointer hover:opacity-90 transition-opacity"
